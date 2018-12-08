4e8aacc2ca3165f1897d5fd7d38346f3033debdc
Christina Deemer <cahdeemer@gmail.com>
2018-12-07 20:30:46 -0500

    added and configured gulp

---
+++ wp-content/themes/mfc/gulpfile.js
@@ -0,0 +1,29 @@
+require('es6-promise').polyfill();
+
+var gulp = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./'))
+  });
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./js/*.js', ['js']);
+});
+
+gulp.task('js', function() {
+    return gulp.src(['./js/*.js'])
+      .pipe(concat('app.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./js'))
+});
+
+// default task
+gulp.task('default', ['sass', 'js', 'watch']);
